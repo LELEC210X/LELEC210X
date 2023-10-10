983a0447de143dfcce2328dc08ec284f16d2ac0b
maistriauxp <pol.maistriaux@uclouvain.be>
2023-10-10 22:51:18 +0200

    Add basic uart to HO audio acq and folder for audio files

---
+++ mcu/hands_on_audio_acquisition/basic-uart-reader.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+uart-reader.py 
+ELEC PROJECT - 210x
+"""
+import argparse
+import serial
+from   serial.tools import list_ports
+import time
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-p", "--port", help="Port for serial communication")
+args = argParser.parse_args()
+print('basic-uart-reader launched...\n')
+
+if args.port is None:
+    print("No port specified, here is a list of serial communication port available")
+    print("================")
+    port = list(list_ports.comports())
+    for p in port:
+        print(p.device)
+    print("================")
+    print("Launch this script with [-p PORT_REF] to access the communication port")
+else :
+    serialPort = serial.Serial(
+        port=args.port, baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE
+    )
+    serialString = ""  # Used to hold data coming over UART
+    while 1:
+        # Wait until there is data waiting in the serial buffer
+        if serialPort.in_waiting > 0:
+
+            # Read data out of the buffer until a carraige return / new line is found
+            serialString = serialPort.readline()
+
+            # Print the contents of the serial data
+            try:
+                print(serialString.decode("Ascii"))
+            except:
+                pass
+                
