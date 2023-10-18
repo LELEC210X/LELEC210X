40ae2b6dc062b8a38f6786aed7ab98810ed64a3f
sarajavadian <sarajava@hotmail.be>
2023-10-18 16:57:51 +0200

    enlever le chain de argu

--- fpga/LimeSDR-Mini_lms7_lelec210x/ip/fir/testbench/mentor/run_sim.tcl
+++
@@ -1,4 +0,0 @@
-source msim_setup_custom.tcl
-ld_debug
-do wave.do
-run 10000ns
--- leaderboard/src/leaderboard/routes/index.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-index = Blueprint("index", __name__, static_folder="../static")
-
-
-@index.route("/")
-def _index():
-    return render_template("index.html")
