c901d8741dd11002eba174db921ce29f7c1767af
sarajavadian <sarajava@hotmail.be>
2023-11-08 16:42:37 +0100

    fork

---
+++ telecom/hands_on_measurements/gr-fsk/python/noise_estimation 3.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2021 UCLouvain.
+#
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+#
+
+
+import time
+
+import numpy as np
+from gnuradio import gr
+
+
+class noise_estimation(gr.basic_block):
+    """
+    docstring for block noise_estimation
+    """
+
+    def __init__(self, n_samples):
+        self.n_samples = n_samples
+        self.noise_est = None
+        self.last_print = 0.0
+
+        gr.basic_block.__init__(
+            self, name="Noise Estimation", in_sig=[np.complex64], out_sig=None
+        )
+
+    def forecast(self, noutput_items, ninput_items_required):
+        ninput_items_required[0] = self.n_samples
+
+    def general_work(self, input_items, output_items):
+        y = input_items[0]
+        if time.time() - self.last_print >= 1.0:
+            self.noise_est = np.mean(np.abs(y) ** 2)
+            print(
+                "[NOISE] Estimated noise power: {} ({}dB, {} samples)".format(
+                    self.noise_est, 10 * np.log10(self.noise_est), len(y)
+                )
+            )
+            self.last_print = time.time()
+
+        self.consume_each(len(y))
+        return 0
---
+++ tex/extract_code 3.py
@@ -0,0 +1,44 @@
+"""
+Scripts for extracting code fragment from a node id.
+
+This is taken from https://github.com/jeertmans/pyextract,
+with minimal amount of code kept.
+
+This is used to include code fragments in LaTeX, that need
+to be re-generated each time.
+
+Usage:
+    python extract_code.py "file.py::path:to:function"
+"""
+import ast
+import sys
+import textwrap
+from pathlib import Path
+
+if __name__ == "__main__":
+    node_id = sys.argv[1]
+
+    try:
+        path_str, parts_str = node_id.split("::", maxsplit=1)
+        path = Path(path_str)
+        parts = parts_str.split("::")
+    except ValueError:
+        path = Path(node_id)
+        parts = []
+
+    code = path.read_text()
+
+    if parts:
+        tree = ast.parse(code)
+
+        for part in parts:
+            try:
+                tree = next(
+                    node for node in tree.body if getattr(node, "name", None) == part
+                )
+            except StopIteration as e:
+                raise ValueError(f"Could not find {part} in {node_id}") from e
+
+        code = ast.get_source_segment(code, tree, padded=True)
+
+    print(textwrap.dedent(code), end="")
