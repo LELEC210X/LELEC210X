6ba4b8f0bd53fc277a8b7a4591fede313c412fae
vctrcc <victor.carballes@student.uclouvain.be>
2025-02-06 15:19:43 +0100

    Correct pickled items are given back from formatter

--- mcu/src/mcu/model_formatter_for_V4.py
+++ mcu/src/mcu/model_formatter_for_V4.py
@@ -1,256 +1,175 @@
 import os
-import matplotlib.pyplot as plt
-import numpy as np
 import pickle
-from sklearn.model_selection import StratifiedKFold, train_test_split
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from typing import Any, Optional, Dict, List
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from sklearn.tree import DecisionTreeClassifier
-
-from classification.datasets import Dataset
-from classification.utils.audio_student import AudioUtil, Feature_vector_DS
-from classification.utils.plots import (
-    plot_decision_boundaries,
-    plot_specgram,
-    show_confusion_matrix,
-)
-
-max_str_length = 20
-def index_to_augmentation(index, distorsion_to_add = ["noise"]):
-    """
-    Take a list of indexes and return the corresponding augmented feature matrix
-    """
-    aug_factor = len(distorsion_to_add)+1
-    nb_sample = len(index)
-    X_aug = np.zeros((aug_factor * nclass * nb_sample, featveclen))
-    y_aug = np.zeros((aug_factor * nclass * nb_sample ), dtype=f"<U{max_str_length}")
-    
-    
-    for s in range(aug_factor):
-        if s == 0:
-            myds.mod_data_aug([])
-        if s != 0:
-            myds.mod_data_aug([distorsion_to_add[s - 1]])
-        count = 0
-        for idx in index:
-            for class_idx, classname in enumerate(classnames):
-                featvec = myds[classname, idx]
-                X_aug[s * nclass * nb_sample + class_idx * nb_sample + count, :] = featvec
-                y_aug[s * nclass * nb_sample + class_idx * nb_sample + count] = classname
-            count += 1
-    
-    return X_aug, y_aug
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.base import BaseEstimator
 
 
-# decision method from a matrix of probabilities probs = np.zeros((n_win, len(classnames))) to a single class
-# first method : naive : take the class with the highest probability in the matrix
-def decision_naive(probs, classnames):
-    index = np.argmax(probs) % len(classnames)
-    return classnames[index]
+####################################################################################################
+# Abstract Classes
 
-# second method : majority vote : take the class with the highest number of votes
-def decision_majority(probs, classnames):
-    n_win = probs.shape[0]
-    votes = np.argmax(probs, axis=1)
-    count = np.bincount(votes)
-    max_indices = np.where(count == count.max())[0]
-    predicted_classes = [classnames[i] for i in max_indices]
-    for prio_class in priority_order:
-        if prio_class in predicted_classes:
-            return prio_class
+class AbstractModelWrapper(ABC):
+    """Abstract base class for a model wrapper."""
 
-# third method : weighted majority vote : take the class with the highest sum of probabilities
-def decision_weighted(probs, classnames):
-    sum_probs = np.sum(probs, axis=0)
-    return classnames[np.argmax(sum_probs)]
+    def __init__(self, model: BaseEstimator, classes: List[str]):
+        self.model = model
+        self.classes = classes
+
+    @abstractmethod
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """Predict the probabilities of the classes."""
+        pass
+
+    @abstractmethod
+    def predict_hist(self, X: List[np.ndarray]) -> np.ndarray:
+        """Predict the probabilities of the classes for the whole history."""
+        pass
 
 
-# fourth method : maximum likelihood : take the class with the product of the probabilities
-def decision_maxlikelihood(probs, classnames):
-    prod_probs = np.prod(probs, axis=0)
-    return classnames[np.argmax(prod_probs)]
+####################################################################################################
+# Model Wrapper Implementation (Example)
 
-dataset = Dataset()
-classnames = dataset.list_classes()
-myds = Feature_vector_DS(dataset, Nft=512, nmel=20, duration=950, shift_pct=0.0)
-nclass = len(classnames)
-naudio = dataset.naudio
-featveclen = len(myds["fire", 0])
-priority_order = ["fire", "handsaw", "chainsaw", "helicopter", "birds"]
+class DecisionTreeWrapper(AbstractModelWrapper):
+    """Decision tree model wrapper that supports pickling."""
 
-# [0] Create a dataset and augment it
-indexes = np.arange(40)
-X, y = index_to_augmentation(indexes, "time_shift")
-X = X/np.linalg.norm(X, axis=1, keepdims=True)
+    def __init__(self, model: DecisionTreeClassifier, classes: List[str]):
+        super().__init__(model, classes)
 
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """Predict class probabilities."""
+        return self.model.predict_proba(X)
 
-# [1] Split the dataset into a training and a test set.
-X_learn, X_test, y_learn, y_test = train_test_split(X, y, test_size=0.3, stratify=y, shuffle=True, random_state=55)
+    def predict_hist(self, X: List[np.ndarray]) -> np.ndarray:
+        """Predict class probabilities over a sequence and return the average."""
+        proba_list = [self.model.predict_proba(x) for x in X]
+        return np.mean(proba_list, axis=0)
 
+    def __getstate__(self):
+        """Ensure picklability by storing only necessary attributes."""
+        return {
+            "model_state": self.model.__getstate__(),
+            "classes": self.classes
+        }
 
-# [2] Model training and selection.
-
-nfolds = 4
-# max_depth = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
-max_depth = [5, 10, 20, 30]
-# min_samples_split = [2,3,4,5,6,7,8,9,10]
-min_samples_split = [2,3,4,5]
-
-
-results = pd.DataFrame(columns=["max_depth","min_samples_split","decision_method","mean_macro_F1_score","std_macro_F1_score"])
-
-# K fold cross validation
-kf = StratifiedKFold(n_splits=nfolds, shuffle=True, random_state=55)
-
-for depth in max_depth:
-    for split in min_samples_split:
-        model = DecisionTreeClassifier(max_depth=depth, min_samples_split=split)
-        F1_scores = []
-        for train_idx, test_idx in kf.split(X, y):
-            X_train, y_train = X[train_idx], y[train_idx]
-            X_test, y_test = X[test_idx], y[test_idx]
-            model.fit(X_train, y_train)
-            predictions = model.predict(X_test)
-            confusion_matrix = np.zeros((len(classnames), len(classnames)))
-            for i in range(len(classnames)):
-                for j in range(len(classnames)):
-                    confusion_matrix[i, j] = np.sum((y_test == classnames[i]) & (predictions == classnames[j]))
-            F1_scores = []
-            for i in range(len(classnames)):
-                TP = confusion_matrix[i, i]
-                FP = np.sum(confusion_matrix[:, i]) - TP
-                FN = np.sum(confusion_matrix[i, :]) - TP
-                precision = TP / (TP + FP)
-                recall = TP / (TP + FN)
-                F1 = 2 * precision * recall / (precision + recall)
-                F1_scores.append(F1)
-        mean_F1 = np.mean(F1_scores)
-        std_F1 = np.std(F1_scores)
-        results = results._append({"max_depth":depth, "min_samples_split":split, "mean_macro_F1_score":mean_F1, "std_macro_F1_score":std_F1}, ignore_index=True)
-        
-# print best hyperparameters and best F1 score
-best_idx = results["mean_macro_F1_score"].idxmax()
-best_depth = results.loc[best_idx, "max_depth"]
-best_depth = int(best_depth)
-best_split = results.loc[best_idx, "min_samples_split"]
-best_split = int(best_split)
-best_F1 = results.loc[best_idx, "mean_macro_F1_score"]
-print("Best hyperparameters: max_depth = {}, min_samples_split = {}".format(best_depth, best_split))
-print("Best F1 score: {}".format(best_F1))
-
-#plot a heatmap of the F1 scores
-results = results.pivot_table(index="max_depth", columns="min_samples_split", values="mean_macro_F1_score")
-plt.figure()
-plt.imshow(results, cmap='coolwarm')  # Change colormap here (e.g., 'viridis', 'plasma', 'cividis', 'coolwarm')
-plt.xlabel("min_samples_split")
-plt.ylabel("max_depth")
-plt.xticks(np.arange(len(min_samples_split)), min_samples_split)
-plt.yticks(np.arange(len(max_depth)), max_depth)
-plt.title("Macro F1 score")
-plt.savefig("hyperparameters_heatmap.pdf")
+    def __setstate__(self, state):
+        """Restore pickled object."""
+        self.model = DecisionTreeClassifier()  # Create a new instance
+        self.model.__setstate__(state["model_state"])  # Restore model state
+        self.classes = state["classes"]
 
 
-# [3] Train the model with the best hyperparameters.
-
-model = DecisionTreeClassifier(max_depth=best_depth, min_samples_split=best_split)
-model.fit(X_learn, y_learn)
-            
+# USER CODE HERE
 
 
-# [4] Evaluate the model on the test set.
-print(len(X_test))
-predictions = model.predict(X_test)
-confusion_matrix = np.zeros((len(classnames), len(classnames)))
-for i in range(len(classnames)):
-    for j in range(len(classnames)):
-        confusion_matrix[i, j] = np.sum((y_test == classnames[i]) & (predictions == classnames[j]))
-        
-F1_scores = []
-for i in range(len(classnames)):
-    TP = confusion_matrix[i, i]
-    FP = np.sum(confusion_matrix[:, i]) - TP
-    FN = np.sum(confusion_matrix[i, :]) - TP
-    precision = TP / (TP + FP)
-    recall = TP / (TP + FN)
-    F1 = 2 * precision * recall / (precision + recall)
-    F1_scores.append(F1)
-    
-mean_F1 = np.mean(F1_scores)
-print("F1 score on test set: {}".format(mean_F1))
 
 
-plt.figure()
-plt.imshow(confusion_matrix, cmap='coolwarm')  # Change colormap here (e.g., 'viridis', 'plasma', 'cividis', 'coolwarm')
-plt.xlabel("Predicted class")
-plt.ylabel("True class")
-plt.xticks(np.arange(len(classnames)), classnames)
-plt.yticks(np.arange(len(classnames)), classnames)
-plt.title("Confusion matrix")
-for i in range(len(classnames)):
-    for j in range(len(classnames)):
-        plt.text(j, i, int(confusion_matrix[i, j]), ha="center", va="center", color="w")
-plt.colorbar()
-plt.savefig("confusion_matrix.pdf")
-plt.show()
 
-#visualize the decision tree
-from sklearn.tree import plot_tree
-plt.figure()
-features_names = ["feature {}".format(i) for i in range(featveclen)]
-plot_tree(model, feature_names=features_names, class_names=classnames, filled=True, fontsize=12, proportion=True)
-plt.savefig("decision_tree.pdf")
-plt.show()
+####################################################################################################
+# Model Serialization Format
 
-# [5] Evaluate the model robustness
-# we choose 30% of the data inside indexes
-index_test = np.random.choice(indexes, int(0.3*len(indexes)), replace=False)
-index_learn = np.setdiff1d(indexes, index_test)
-X_learn, y_learn = index_to_augmentation(index_learn, distorsion_to_add = ["time_shift"])
-X_test, y_test = index_to_augmentation(index_test, distorsion_to_add = ["echo"])
-X_learn = X_learn/np.linalg.norm(X_learn, axis=1, keepdims=True)
-X_test = X_test/np.linalg.norm(X_test, axis=1, keepdims=True)
+@dataclass
+class ModelPickleFormat:
+    """Data structure for storing model metadata and parameters."""
+    model_state: Dict[str, Any]  # Pickled model state
+    classes: List[str]           # List of class labels
+    mel_len: int                 # Mel vector length
+    mel_num: int                 # Number of mel vectors
+    mel_flat: bool               # Whether mel vectors are flattened
+    needs_hist: bool             # Whether model needs history
+    concat_hist: bool            # Whether history is concatenated
+    num_hist: int                # Number of history elements
 
-model = DecisionTreeClassifier(max_depth=best_depth, min_samples_split=best_split)
-model.fit(X_learn, y_learn)
-predictions = model.predict(X_test)
+    def to_dict(self) -> Dict[str, Any]:
+        """Convert the model format to a dictionary."""
+        return self.__dict__
 
-confusion_matrix = np.zeros((len(classnames), len(classnames)))
-for i in range(len(classnames)):
-    for j in range(len(classnames)):
-        confusion_matrix[i, j] = np.sum((y_test == classnames[i]) & (predictions == classnames[j]))
-        
-F1_scores = []
-for i in range(len(classnames)):
-    TP = confusion_matrix[i, i]
-    FP = np.sum(confusion_matrix[:, i]) - TP
-    FN = np.sum(confusion_matrix[i, :]) - TP
-    precision = TP / (TP + FP)
-    recall = TP / (TP + FN)
-    F1 = 2 * precision * recall / (precision + recall)
-    F1_scores.append(F1)
-    
-mean_F1 = np.mean(F1_scores)
-print("F1 score on test set: {}".format(mean_F1))
-
-plt.figure()
-plt.imshow(confusion_matrix, cmap='coolwarm')  # Change colormap here (e.g., 'viridis', 'plasma', 'cividis', 'coolwarm')
-plt.xlabel("Predicted class")
-plt.ylabel("True class")
-plt.xticks(np.arange(len(classnames)), classnames)
-plt.yticks(np.arange(len(classnames)), classnames)
-plt.title("Confusion matrix")
-for i in range(len(classnames)):
-    for j in range(len(classnames)):
-        plt.text(j, i, int(confusion_matrix[i, j]), ha="center", va="center", color="w")
-plt.colorbar()
-plt.savefig("confusion_matrix_robustness.pdf")
-plt.show()
-    
- 
+    @staticmethod
+    def from_dict(model_dict: Dict[str, Any]) -> Optional["ModelPickleFormat"]:
+        """Convert a dictionary back into a ModelPickleFormat object."""
+        required_keys = [
+            "model_state", "classes", "mel_len", "mel_num", 
+            "mel_flat", "needs_hist", "concat_hist", "num_hist"
+        ]
+        if not all(key in model_dict for key in required_keys):
+            return None
+        return ModelPickleFormat(**model_dict)
 
 
-# [6] Save the trained model
-model_dir = "data/models/"
-filename = "model.pickle"
-model = DecisionTreeClassifier(max_depth=best_depth, min_samples_split=best_split)
-model.fit(X, y)
-pickle.dump(model, open(model_dir + filename, "wb"))
+####################################################################################################
+# Model Persistence Functions
+
+def save_model(wrapper: DecisionTreeWrapper, model_format: ModelPickleFormat, path: str):
+    """Save the model and its metadata to a pickle file."""
+    model_format_dict = model_format.to_dict()
+    model_format_dict["model_state"] = wrapper.__getstate__()
+
+    with open(path, "wb") as file:
+        pickle.dump(model_format_dict, file)
+
+
+def load_model(path: str) -> Optional[DecisionTreeWrapper]:
+    """Load the model from a pickle file."""
+    with open(path, "rb") as file:
+        model_dict = pickle.load(file)
+
+    model_format = ModelPickleFormat.from_dict(model_dict)
+    if model_format is None:
+        return None
+
+    # Restore the model
+    restored_model = DecisionTreeWrapper(DecisionTreeClassifier(), model_format.classes)
+    restored_model.__setstate__(model_dict["model_state"])
+
+    return restored_model
+
+
+####################################################################################################
+# Example Usage
+
+if __name__ == "__main__":
+    # Define model and class labels
+    model_to_use = DecisionTreeClassifier(max_depth=5, min_samples_split=2)
+    classes = ["class1", "class2", "class3"]
+
+    # Generate sample training data
+    X = np.random.rand(40, 400)
+    y = np.random.choice(classes, 40)
+
+    # Modify data for class1
+    X[y == "class1"] = np.ones(400) / 2
+
+    # Train the model
+    model_to_use.fit(X, y)
+
+    # Wrap the trained model
+    model_wrapper = DecisionTreeWrapper(model_to_use, classes)
+
+    # Define model metadata
+    model_format = ModelPickleFormat(
+        model_state=model_wrapper.__getstate__(),
+        classes=classes,
+        mel_len=20,
+        mel_num=20,
+        mel_flat=True,
+        needs_hist=True,
+        concat_hist=False,
+        num_hist=3
+    )
+
+    # Save the model
+    save_model(model_wrapper, model_format, "model.pickle")
+
+    # Load the model
+    loaded_model = load_model("model.pickle")
+
+    # Test the loaded model
+    if loaded_model:
+        X_test = np.random.rand(3, 400)
+        print(loaded_model.predict(X_test[0].reshape(1, -1)))
+        print(loaded_model.predict_hist([X_test[i].reshape(1, -1) for i in range(len(X_test))]))
